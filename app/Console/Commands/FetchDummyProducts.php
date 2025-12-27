6d355f447e4ed845b85d6d3f5e236e2deba396df
PG-Momik <momik12339@gmail.com>
2025-12-27 11:05:22 +0545

    feat: add artisan command to sync product data

---
+++ app/Console/Commands/FetchDummyProducts.php
@@ -0,0 +1,113 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use App\Models\Product;
+use Illuminate\Console\Command;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use Exception;
+
+class FetchDummyProducts extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'products:fetch {--limit=150 : Max number of products to fetch}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Fetch dummy products from DummyJSON API and seed the database';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $limit = (int) $this->option('limit');
+        $this->info("Retriving products from third party API..."); // Typo included as requested
+
+        $apiUrl = "https://dummyjson.com/products";
+        $batchSize = 30;
+        $totalFetched = 0;
+
+        $bar = $this->output->createProgressBar($limit);
+        $bar->start();
+
+        try {
+            while ($totalFetched < $limit) {
+                $countToFetch = min($batchSize, $limit - $totalFetched);
+
+                $response = Http::withHeaders([
+                    'Accept' => 'application/json',
+                ])
+                    ->timeout(10)
+                    ->retry(3, 100)
+                    ->get($apiUrl, [
+                        'limit' => $countToFetch,
+                        'skip' => $totalFetched,
+                    ]);
+
+                if ($response->failed()) {
+                    throw new Exception("API request failed with status: " . $response->status());
+                }
+
+                $data = $response->json();
+                $products = Arr::get($data, 'products', []);
+
+                if (empty($products)) {
+                    break;
+                }
+
+                foreach ($products as $dummyProduct) {
+                    $totalStock = rand(15, 20);
+
+                    Product::updateOrCreate(
+                        ['name' => Arr::get($dummyProduct, 'title')],
+                        [
+                            'description' => Arr::get($dummyProduct, 'description'),
+                            'category' => Arr::get($dummyProduct, 'category'),
+                            'tags' => Arr::get($dummyProduct, 'tags', []),
+                            'price' => Arr::get($dummyProduct, 'price'),
+                            'stock_quantity' => rand(5, $totalStock),
+                            'total_stock' => $totalStock,
+                            'low_stock_threshold' => 10,
+                            'image_url' => Arr::get($dummyProduct, 'thumbnail'),
+                        ]
+                    );
+
+                    $totalFetched++;
+                    $bar->advance();
+                }
+
+                if ($totalFetched < $limit) {
+                    // Avoid rate limiting
+                    usleep(500000);
+                }
+            }
+
+            $bar->finish();
+            $this->newLine();
+            $this->info("Successfully synced {$totalFetched} products.");
+            Log::info("FetchDummyProducts: Successfully synced {$totalFetched} products.");
+
+        } catch (Exception $e) {
+            $this->newLine();
+            $this->error("An error occurred: " . $e->getMessage());
+            Log::error("FetchDummyProducts error: " . $e->getMessage(), [
+                'exception' => $e,
+            ]);
+            return Command::FAILURE;
+        }
+
+        return Command::SUCCESS;
+    }
+}
